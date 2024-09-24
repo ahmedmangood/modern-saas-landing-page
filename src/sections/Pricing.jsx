ea9f3806aa8708ca96ccef30cca6efc84858c28a
ahmedmangood <ahmedmangood656@gmail.com>
2024-09-24 14:52:26 +0300

    finished pricing section

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Element } from "react-scroll";
 import clsx from "clsx";
-
+import { plans } from "../constants/index.jsx";
+import CountUp from "react-countup";
+import Button from "../components/Button.jsx";
 const Pricing = () => {
   const [monthly, setMonthly] = useState(false);
 
@@ -45,6 +47,139 @@ const Pricing = () => {
                 )}
               />
             </div>
+            <div className={"pricing-bg"}>
+              <img
+                src={"/images/bg-outlines.svg"}
+                width={960}
+                height={380}
+                alt={"outline"}
+                className={"relative z-2"}
+              />
+              <img
+                src={"/images/bg-outlines-fill.png"}
+                width={960}
+                height={380}
+                alt={"outline"}
+                className={"absolute inset-0 opacity-5 mix-blend-soft-light"}
+              />
+            </div>
+          </div>
+          {/*  Pricing Section */}
+          <div
+            className={
+              "scroll-hide relative z-2 flex -mt-12 items-start max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6"
+            }
+          >
+            {plans.map((plan, index) => {
+              const isPrimaryPrice = index === 1;
+              return (
+                <div
+                  className={
+                    "pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
+                  }
+                  key={plan.id}
+                >
+                  {isPrimaryPrice && (
+                    <div
+                      className={
+                        "g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl"
+                      }
+                    />
+                  )}
+                  <div
+                    className={clsx(
+                      "absolute left-0 right-0 z-2 flex items-center justify-center",
+                      isPrimaryPrice ? "-top-6" : "-top-6 xl:-top-11",
+                    )}
+                  >
+                    <img
+                      src={plan.logo}
+                      alt={plan.title}
+                      className={clsx(
+                        "object-contain drop-shadow-2xl",
+                        isPrimaryPrice ? "size-[120px]" : "size-[88px]",
+                      )}
+                    />
+                  </div>
+                  <div
+                    className={clsx(
+                      "relative flex flex-col items-center",
+                      isPrimaryPrice ? "pt-24" : "pt-12",
+                    )}
+                  >
+                    <div
+                      className={clsx(
+                        "small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase",
+                        isPrimaryPrice
+                          ? "border-p3 text-p3"
+                          : "border-p1 text-p1",
+                      )}
+                    >
+                      {plan.title}
+                    </div>
+                    <div
+                      className={
+                        "relative z-2 flex items-center justify-center"
+                      }
+                    >
+                      <div
+                        className={clsx(
+                          "h-num flex items-start",
+                          isPrimaryPrice ? "text-p3" : "text-p4",
+                        )}
+                      >
+                        ${" "}
+                        <CountUp
+                          start={plan.priceMonthly}
+                          end={monthly ? plan.priceMonthly : plan.priceYearly}
+                          duration={0.4}
+                          useEasing={false}
+                          preserveValue
+                        />
+                      </div>
+                      <div className={"small-1 relative top-3 ml-1 uppercase"}>
+                        /mo
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className={clsx(
+                      "body-1 relative z-2 mb-10 w-full border-b-s2 pb-9 text-center text-p4",
+                      isPrimaryPrice && "border-b",
+                    )}
+                  >
+                    {plan.caption}
+                  </div>
+                  <ul className={"mx-auto space-y-4 xl:px-7"}>
+                    {plan.features.map((feature) => (
+                      <li
+                        key={feature}
+                        className={"relative flex items-center gap-5"}
+                      >
+                        <img
+                          src={"/images/check.png"}
+                          alt={"check"}
+                          className={"size-10 object-contain"}
+                        />
+                        <p className={"flex-1"}>{feature}</p>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className={"mt-10 flex w-full justify-center"}>
+                    <Button icon={plan.icon}>Get Started</Button>
+                  </div>
+                  {isPrimaryPrice && (
+                    <p
+                      className={
+                        "small-compact mt-9 text-center text-p3 before:content-['-'] before:mr-2 after:content-['-'] after:ml-2"
+                      }
+                    >
+                      Limited time offer
+                    </p>
+                  )}
+                </div>
+              );
+            })}
           </div>
         </div>
       </Element>
